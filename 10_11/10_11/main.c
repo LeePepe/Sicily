64c67c7da2fe90a5e679fe6c82ae4f01fd8c0f4b
LeePepe <1105208297@qq.com>
2015-10-13 11:39:52 +0800

    wrote a read file for C dir

---
+++ 10_11/10_11/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  10_11
+//
+//  Created by 李天培 on 15/10/11.
+//  Copyright © 2015年 lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Git is version control system.
+C is sicily problems answer.
+Every .c name is sicily identifier.
