3a949b2142c9b9022dc900b27b20e4cf6f7150b6
LeePepe <1105208297@qq.com>
2015-10-13 20:51:08 +0800

    1007 To and Fro

--- 1007.cpp
+++ 1007.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 
 using namespace std;
-
 int main() {
     int n;
     while (cin >> n && n != 0) {
-      string s;
-      char letter[20][20];
-      cin >> s;
-      for (int i = 0; i < s.length() / n; i++) {
-	for (int j = 0; j < n; j++) {
-	  letter[i][j] = s[i * n + j];
-	}
-      }
-
-      for (int i = 0; i < s.length() / n; i++) {
-	if (i % 2 == 1) {
-	  for (int j = 0; j < n / 2; j++) {
-	    char c = letter[i][j];
-	    letter[i][j] = letter[i][n - 1 - j];
-	    letter[i][n - 1 - j] = c;
-	  }
-	}
-      }       
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < s.length() / n; j++) {
-	  cout << letter[j][i];
-	}
-      }
-      cout << endl;
+        string s;
+        char letter[20][20];
+        cin >> s;
+        for (int i = 0; i < s.length() / n; i++) {
+            for (int j = 0; j < n; j++) {
+                letter[i][j] = s[i * n + j];
+            }
+        }
+        
+        for (int i = 0; i < s.length() / n; i++) {
+            if (i % 2 == 1) {
+                for (int j = 0; j < n / 2; j++) {
+                    char c = letter[i][j];
+                    letter[i][j] = letter[i][n - 1 - j];
+                    letter[i][n - 1 - j] = c;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < s.length() / n; j++) {
+                cout << letter[j][i];
+            }
+        }
+        cout << endl;
     }
     return 0;
 }
+
